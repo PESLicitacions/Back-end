31030c3e34b4b32fe3796831e2010db2672f8cf0
paucp1 <paucp1@users.noreply.github.com>
2023-03-23 10:48:45 +0100

    added new models and urls in backend

--- src/licitacions/models.py
+++ src/licitacions/models.py
@@ -1,9 +1,32 @@
 from django.db import models
+from licitacions import choices
 
 # Create your models here.
-class Licitacio(models.Model):
-    procediment = models.CharField(max_length=50)
-    fase_publicacio = models.CharField(max_length=80)
+
+class Localitzacio(models.Model):
+    nom = models.CharField(max_length=100, primary_key=True)
+    longitud = models.DecimalField(max_digits=22, decimal_places=16)
+    latitud = models.DecimalField(max_digits=22, decimal_places=16)
+
+
+class Ambit(models.Model):
+    codi = models.IntegerField(primary_key=True)
+    nom = models.CharField(max_length=100, verbose_name="nom àmbit")
+
+
+class Departament(models.Model):
+    codi = models.IntegerField(primary_key=True)
+    nom = models.CharField(max_length=100, verbose_name="nom departament")
+
+
+class Organ(models.Model):
+    codi = models.IntegerField(primary_key=True)
+    nom = models.CharField(max_length=100, verbose_name="nom organ")
+
+
+class LicitacioPublica(models.Model):
+    procediment = models.CharField(max_length=150, choices=choices.procediments)
+    fase_publicacio = models.CharField(max_length=80, choices=choices.fase_publicacio)
     denominacio = models.TextField()
     objecte_contracte = models.TextField()
     pressupost = models.DecimalField(decimal_places=2, max_digits=9)
@@ -19,5 +42,28 @@ class Licitacio(models.Model):
     resultat = models.CharField(max_length=50)
     data_adjudicacio_contracte = models.DateField()
     data_formalitzacio_contracte = models.DateField()
+    enllaç = models.URLField()
+    lloc_execucio = models.ForeignKey(Localitzacio, to_field='nom' , related_name="licitacio_publica", null=True, on_delete=models.SET_NULL)
+
+
+class LicitacioPrivada(models.Model):
+    procediment = models.CharField(max_length=150, choices=choices.procediments)
+    fase_publicacio = models.CharField(max_length=80, choices=choices.fase_publicacio)
+    denominacio = models.TextField()
+    objecte_contracte = models.TextField()
+    pressupost = models.DecimalField(decimal_places=2, max_digits=9)
+    valor_estimat_contracte = models.DecimalField(decimal_places=2, max_digits=9)
+    duracio_contracte = models.CharField(max_length=80)
+    termini_presentacio_ofertes = models.DateTimeField()
+    data_publicacio_anunci = models.DateTimeField()
+    data_publicacio_adjudicacio = models.DateTimeField()
+    codi_cpv = models.IntegerField()
+    import_adjudicacio_sense_iva = models.DecimalField(decimal_places=2, max_digits=9)
+    import_adjudicacio_amb_iva = models.DecimalField(decimal_places=2, max_digits=9)
+    ofertes_rebudes = models.IntegerField()
+    resultat = models.CharField(max_length=50)
+    data_adjudicacio_contracte = models.DateField()
+    data_formalitzacio_contracte = models.DateField()
+    lloc_execucio = models.ForeignKey(Localitzacio, related_name="licitacio_privada", null=True, on_delete=models.SET_NULL)
 
 
--- src/licitacions/urls.py
+++ src/licitacions/urls.py
@@ -1,8 +1,15 @@
 from rest_framework import routers
-from .api import LicitacioViewSet
+from licitacions import api
 
 router = routers.DefaultRouter()
 
-router.register('api/licitacions', LicitacioViewSet, 'licitacions')
+router.register('api/licitacions_publiques', api.LicitacioPublicaViewSet, 'licitacions_publiques')
+router.register('api/licitacions_privades', api.LicitacioPrivadaViewSet, 'licitacions_privades')
+router.register('api/localitzacions', api.LocalitzacioViewSet, 'localitzacions')
+router.register('api/ambit', api.AmbitViewSet, 'ambits')
+router.register('api/departament', api.DepartamentViewSet, 'departaments')
+router.register('api/organ', api.OrganViewSet, 'organs')
+router.register('api/location_barcelona', api.BarcelonaViewSet, 'barna')
+
 
 urlpatterns = router.urls
