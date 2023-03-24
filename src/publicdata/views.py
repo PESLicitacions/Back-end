21f3cd3d872316bba4e5f7c7429e082fbe6393fd
santilara <santilaracelis16@gmail.com>
2023-03-24 16:28:27 +0100

    fixed request

--- src/publicdata/views.py
+++ src/publicdata/views.py
@@ -7,11 +7,10 @@ from django.http import JsonResponse
 def get_data(request):
     context = {}
     
-    num_rows = 20
-    base_url = 'https://analisi.transparenciacatalunya.cat/resource/a23c-d6vp.json?$limit=20'
-    
+    num_rows = '212594'
+    base_url = 'https://analisi.transparenciacatalunya.cat/resource/a23c-d6vp.json?$limit=' + num_rows
     response_API = requests.get(base_url)
     data = response_API.text
     parse_json = json.loads(data)
     print(len(parse_json))
-    return JsonResponse(data, safe=false)
+    return JsonResponse(data, safe=False)
