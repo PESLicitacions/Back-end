f20c54aaa2611d5e3b069900c0b161810b24469d
paucp1 <paucp1@users.noreply.github.com>
2023-03-31 11:28:57 +0200

    filters adapted

--- src/api/urls.py
+++ src/api/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path('licitacions_publiques', views.LicitacionsPubliquesList.as_view()),
     path('licitacions_publiques/<int:pk>/', views.LicitacioPublicaDetail.as_view()),
     path('licitacions_privades', views.LicitacionsPrivadesList.as_view()),
-    path('licitacions_privades/<int:pk>/', views.LicitacioPrivadaDetail.as_view())
+    path('licitacions_privades/<int:pk>/', views.LicitacioPrivadaDetail.as_view()),
+    path('localitzacions', views.LocalitzacionsInfo.as_view()),
+    path('ambits', views.AmbitsInfo.as_view()),
+    path('departaments', views.DepartamentsInfo.as_view()),
+    path('organs', views.OrgansInfo.as_view()),
+    path('tipus_contracte', views.TipusContracteInfo.as_view())
 ]
--- src/api/views.py
+++ src/api/views.py
@@ -1,9 +1,7 @@
-from rest_framework.response import Response
-from rest_framework import status
-from django.http import Http404
-from rest_framework.views import APIView
 from rest_framework import generics
 from django.db.models import Q
+from rest_framework.pagination import LimitOffsetPagination
+
 
 from licitacions.models import *
 from licitacions.serializers import *
@@ -18,6 +16,7 @@ class LicitacionsPubliquesList(generics.ListAPIView):
         localitzacio = self.request.query_params.get('lloc_execucio')
         if localitzacio is not None:
             queryset = queryset.filter(lloc_execucio__nom__icontains=localitzacio)
+            #queryset = queryset.filter(lloc_execucio_id=localitzacio)
 
         pressupost_min = self.request.query_params.get('pressupost_min')
         if pressupost_min is not None:
@@ -30,18 +29,22 @@ class LicitacionsPubliquesList(generics.ListAPIView):
         ambit = self.request.query_params.get('ambit')
         if ambit is not None:
             queryset = queryset.filter(ambit__nom__icontains=ambit)
+            #queryset = queryset.filter(ambit_id=ambit)
 
         departament = self.request.query_params.get('departament')
         if departament is not None:
             queryset = queryset.filter(departament__nom__icontains=departament)
+            #queryset = queryset.filter(departament_id=departament)
 
         organ = self.request.query_params.get('organ')
         if organ is not None:
             queryset = queryset.filter(organ__nom__icontains=organ)
+            #queryset = queryset.filter(organ_id=organ)
         
         tipus_contracte = self.request.query_params.get('tipus_contracte')
         if tipus_contracte is not None:
-            queryset = queryset.filter(Q(tipus_contracte__tipus_contracte__icontains=tipus_contracte) | Q(tipus_contracte__subtipus_contracte__icontains=tipus_contracte))
+            #queryset = queryset.filter(Q(tipus_contracte__tipus_contracte__icontains=tipus_contracte) | Q(tipus_contracte__subtipus_contracte__icontains=tipus_contracte))
+            queryset = queryset.filter(tipus_contracte_id=tipus_contracte)
         
         duracio_min = self.request.query_params.get('duracio_min')
         if duracio_min is not None:
@@ -58,7 +61,7 @@ class LicitacionsPubliquesList(generics.ListAPIView):
         data_fi = self.request.query_params.get('data_fi')
         if data_fi is not None:
             queryset = queryset.filter(data_fi__lte=data_fi)
-            
+
         return queryset
     
         
@@ -66,11 +69,77 @@ class LicitacioPublicaDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = LicitacioPublica.objects.all()
     serializer_class = LicitacioPublicaDetailsSerializer
 
+
 class LicitacionsPrivadesList(generics.ListCreateAPIView):
     queryset = LicitacioPrivada.objects.all()
     serializer_class = LicitacioPrivadaPreviewSerializer
+
 
 class LicitacioPrivadaDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = LicitacioPrivada.objects.all()
     serializer_class = LicitacioPrivadaDetailsSerializer
 
+
+class LocalitzacionsInfo(generics.ListAPIView):
+    serializer_class = LocalitzacioInfoSerializer
+    pagination_class = None
+
+    def get_queryset(self):
+        queryset = Localitzacio.objects.all()
+
+        localitzacio = self.request.query_params.get('lloc_execucio')
+        if localitzacio is not None:
+            queryset = queryset.filter(nom__icontains=localitzacio)
+        return queryset
+
+
+class AmbitsInfo(generics.ListAPIView):
+    serializer_class = AmbitInfoSerializer
+    pagination_class = None
+
+    def get_queryset(self):
+        queryset = Ambit.objects.all()
+
+        ambit = self.request.query_params.get('ambit')
+        if ambit is not None:
+            queryset = queryset.filter(nom__icontains=ambit)
+        return queryset
+
+
+class DepartamentsInfo(generics.ListAPIView):
+    serializer_class = DepartamentInfoSerializer
+    pagination_class = None
+
+    def get_queryset(self):
+        queryset = Departament.objects.all()
+
+        departament = self.request.query_params.get('departament')
+        if departament is not None:
+            queryset = queryset.filter(nom__icontains=departament)
+        return queryset
+    
+
+class OrgansInfo(generics.ListAPIView):
+    serializer_class = OrganInfoSerializer
+    pagination_class = None
+
+    def get_queryset(self):
+        queryset = Organ.objects.all()
+
+        organ = self.request.query_params.get('organ')
+        if organ is not None:
+            queryset = queryset.filter(nom__icontains=organ)
+        return queryset
+
+
+class TipusContracteInfo(generics.ListAPIView):
+    serializer_class = TipusContracteInfoSerializer
+    pagination_class = None
+    
+    def get_queryset(self):
+        queryset = TipusContracte.objects.all()
+
+        tipus_contracte = self.request.query_params.get('tipus_contracte')
+        if tipus_contracte is not None:
+            queryset = queryset.filter(Q(tipus_contracte__icontains=tipus_contracte) | Q(subtipus_contracte__icontains=tipus_contracte))
+        return queryset
