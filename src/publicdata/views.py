450e2efea6f838e80c068519ed19ed3b9a9accf0
santilara16 <125558990+santilara16@users.noreply.github.com>
2023-06-11 23:56:23 +0200

    Update views.py

--- src/publicdata/views.py
+++ src/publicdata/views.py
@@ -30,7 +30,7 @@ def test():
 
 def get_data():
     PARAMS = 'procediment, fase_publicacio, denominacio, objecte_contracte, pressupost_licitacio, valor_estimat_contracte, duracio_contracte, termini_presentacio_ofertes, data_publicacio_anunci, data_publicacio_adjudicacio, codi_cpv, import_adjudicacio_sense, import_adjudicacio_amb_iva, ofertes_rebudes, resultat, data_adjudicacio_contracte, data_formalitzacio_contracte, enllac_publicacio, lloc_execucio, codi_ambit, nom_ambit, codi_departament_ens, nom_departament_ens, codi_organ, nom_organ, tipus_contracte, subtipus_contracte'
-    num_rows = '200000'
+    num_rows = '5000'
     base_url = 'https://analisi.transparenciacatalunya.cat/resource/a23c-d6vp.json?$query=SELECT ' + PARAMS + ' LIMIT ' + num_rows
     response_API = requests.get(base_url)
     data = response_API.text
